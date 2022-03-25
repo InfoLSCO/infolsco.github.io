add29cc47ca27ccef3d0ecf8c31834f46f27c3de
InfoLSCO <86148867+InfoLSCO@users.noreply.github.com>
2022-03-25 14:31:57 +0100

    Update

---
+++ chatty/index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+
+    <head>
+
+        <link rel="icon" href="../img/icon.png" type="image/x-icon">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Lato&family=Open+Sans:ital,wght@0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Lato&family=Open+Sans:ital,wght@0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700&family=Raleway&display=swap" rel="stylesheet">
+
+        <title>LsCo Chatty - github.io</title>
+
+    </head>
+
+        <body>
+
+                <div> 
+
+                    <center>
+
+                        <u>
+
+                            <center class="title">
+
+                                <h1>Chatty Settings</h1>
+            
+                            </center>
+
+                        </u>
+
+                        <div>
+
+                            <h4>
+                                Meine Chatty Settings!
+                            </h4>
+
+                            <h4>
+                                bei Fragen, kann man mir auf Discord Schreiben.
+                            </h4>
+
+                            <h4>
+                                DC: 
+                                <u>
+                                    Lennox | LsCo#5877
+                                </u>
+                            </h4>
+
+                            <h5>
+
+                                <u>
+                                    Updated at: 26.03.2022
+                                </u>
+
+                            </h5>
+
+                            <center class="title">
+
+                                <h2>
+                                    <u>
+                                        Infos
+                                    </u>
+                                </h2>
+
+                                <h4>
+                                    Du kannst auf dem Text Klicken, um das Bild in ein Neuen Tab zu öffnen.
+                                </h4>
+
+                                <h4>
+                                    Page Inspired: Louis (<a style="color: rgba(0, 208, 255, 0.849);" href="https://www.twitch.tv/louis6321/">Twitch</a>, <a style="color: rgba(0, 208, 255, 0.849);" href="https://chatty.louis.id.au/">Webseite</a>)
+                                </h4>
+            
+                            </center>
+
+                        </div>
+
+                    </center>
+
+                    <center>
+
+                        <h5><a style="color: #FFF;" href="./img/main.png">1. Main</a></h5>
+                        <img src="./img/main.png" class="bild">
+
+                        <h5><a style="color: #FFF;" href="./img/look.png">2. Look</a></h5>
+                        <img src="./img/look.png" class="bild">
+
+                        <h5><a style="color: #FFF;" href="./img/chat-colors.png">3. Chat Colors</a></h5>
+                        <img src="./img/chat-colors.png" class="bild">
+
+                        <h5><a style="color: #FFF;" href="./img/msg-colors.png">4. Msg. Colors</a></h5>
+                        <img src="./img/msg-colors.png" class="bild">
+
+                        <h4>DeepSkyBlue,1,#333333,0,config:info start:[Info]</h4>
+
+                        <h4>DeepSkyBlue,1,#333333,0,config:info start:[Notification]</h4>
+
+                        <h4>White,1,#080B42,1,config:repeatedmsg</h4>
+
+                        <h4>White,1,#006666,1,config:t|first-msg=1</h4>
+
+                        <h4>White,1,#330033,1,config:info start:[AutoMod]</h4>
+
+                        <h4>White,1,#003165,1,config:info start:[ModAction]</h4>
+
+                        <h4>White,1,#990099,1,config:info start:[Points]</h4>
+
+                        <h5><a style="color: #FFF;" href="./img/usercolors.png">5. Usercolors</a></h5>
+                        <img src="./img/usercolors.png" class="bild">
+
+                        <h5><a style="color: #FFF;" href="./img/badges.png">6. Badges</a></h5>
+                        <img src="./img/badges.png" class="bild">
+
+                        <h5><a style="color: #FFF;" href="./img/emoticons.png">7. Emoticons</a></h5>
+                        <img src="./img/emoticons.png" class="bild">
+
+                        <h5><a style="color: #FFF;" href="./img/fonts.png">8. Fonts</a></h5>
+                        <img src="./img/fonts.png" class="bild">
+
+                        <h5><a style="color: #FFF;" href="./img/chat.png">9. Chat</a></h5>
+                        <img src="./img/chat.png" class="bild">
+
+                        <h5><a style="color: #FFF;" href="./img/messages.png">10. Messages</a></h5>
+                        <img src="./img/messages.png" class="bild">
+
+                        <h5><a style="color: #FFF;" href="./img/moderation.png">11. Moderation</a></h5>
+                        <img src="./img/moderation.png" class="bild">
+
+                        <h5><a style="color: #FFF;" href="./img/moderation_message-history.png"></a></h5>
+                        <img src="./img/moderation_message-history.png" class="bild">
+
+                    </center>
+
+                </div>
+        </body>
+
+<style>
+
+    body {
+        background-color: #1d1e20;
+        color: #FFF;
+        font-family: 'Lato', sans-serif;
+        font-family: 'Open Sans', sans-serif;
+        line-height: 1;
+        list-style: none;
+    }
+    .title {
+        color: #FFF;
+        user-select: none;
+    }
+    .bild {
+        user-select: none;
+        border-left: 20px;
+        border-right: 20px;
+        border-radius: 20px;
+    }
+    .border {
+        border: solid; 
+        font-size: 17px;
+        border-left: 20px;
+        border-right: 20px;
+        border-radius: 10px; 
+        border-color: #FFF;
+        display: inline-block;
+    }
+
+</style>
+
+<script>
+
+</script>
