e00c928e1457e9245c68ded3c870825c0dd70474
InfoLSCO <86148867+InfoLSCO@users.noreply.github.com>
2022-03-25 14:30:40 +0100

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+
+    <head>
+
+        <link rel="icon" href="../img/icon.png" type="image/x-icon">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Lato&family=Open+Sans:ital,wght@0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Lato&family=Open+Sans:ital,wght@0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700&family=Raleway&display=swap" rel="stylesheet">
+
+        <title>LsCo Main - github.io</title>
+
+    </head>
+
+        <body>
+
+                <div> 
+
+                    <center>
+
+                        <u>
+
+                            <center class="title">
+
+                                <h1>About me</h1>
+            
+                            </center>
+
+                        </u>
+
+                        <div>
+                            <h4>
+                                Lennox aka. (LsCo) 14 Jahre alt. 
+                            </h4>
+
+                            <h4>
+                                Mache Twitch Mod Stuff. 
+                            </h4>
+
+                            <h4>
+                                Dev Zeit ca. Aug. 21
+                            </h4>
+                        </div>
+
+                        <u>
+
+                            <center class="title">
+
+                                <h1>About Moderator</h1>
+            
+                            </center>
+
+                        </u>
+
+                        <div>
+
+                            <h4>
+                                
+                            </h4>
+
+                                <h3>
+                                    <a class="none-copy" href="https://modlookup.3v.fi/u/LsCo" style="color: #FFF;">ModLookUp</a>
+                                </h3>
+
+                                <div class="border">
+        
+                                    <h4>
+                                        Mod in 14 Channels - Views total: 1 810 181
+                                    </h4>
+                                    <h4>
+                                        Follow total: 259 436 - Partners: 2
+                                    </h4>
+
+                                </div>
+
+                        </div>
+
+                        <u>
+
+                            <center class="title">
+                
+                                <h1>Projects</h1>
+                
+                            </center>
+
+                        </u>
+
+                        <div>
+                            <h4 class="none-copy">
+                                None...
+                            </h4>
+                        </div>
+
+                        <u>
+
+                            <center class="title">
+                
+                                <h1>Pages</h1>
+                
+                            </center>
+
+                        </u>
+
+                        <div>
+
+                            <h4><a class="none-copy" href="./chatty" style="color: #FFF;">Chatty</a> </h4>
+                            <h4></h4>
+
+                        </div>
+
+                    </center>
+
+                </div>
+
+        </body>
+
+<style>
+
+body {
+    background-color: #1d1e20;
+    color: #FFF;
+    font-family: 'Lato', sans-serif;
+    font-family: 'Open Sans', sans-serif;
+}
+.title {
+    color: #FFF;
+    user-select: none;
+}
+.border {
+    border: solid; 
+    font-size: 17px;
+    border-left: 20px;
+    border-right: 20px;
+    border-radius: 10px; 
+    border-color: #FFF;
+    display: inline-block;
+
+}
+.none-copy {
+    user-select: none;
+}
+
+</style>
+
+<script>
+    
+</script>
+
+
